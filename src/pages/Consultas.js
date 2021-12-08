3750ef20e4be328730df69a2596e96ff55615fa5
unknown <dfvega@uninorte.edu.co>
2021-12-08 14:06:21 -0500

    correcion_Sprint3

--- src/pages/Consultas.js
+++ src/pages/Consultas.js
@@ -6,6 +6,8 @@ import { AiFillDollarCircle,
   AiFillMoneyCollect,
   AiFillBank,
   AiOutlineSearch }from "react-icons/ai";
+import BarraInferior from "../components/BarraInferior";
+import BarraSuperior from "../components/BarraSuperior";
   class Consultas extends React.Component {
     constructor() {
       super();
@@ -18,31 +20,7 @@ import { AiFillDollarCircle,
     }
     render(){
       return(<body>
-        {/* <!-- ======= Header ======= --> */}
-        <header id="header" className="fixed-top d-flex align-items-center">
-        <div className="container d-flex align-items-center justify-content-between">
-  
-          <div className="logo">
-            <Link to="/index"><img src="assets_general/img/citi.png" alt="" className="img-fluid"/></Link>
-            <a style={{fontWeight: "bold", paddingLeft: "30px", paddingTop: "10px"}}>Bienvenido Usuario X</a>
-          </div>
-  
-          <nav id="navbar" className="navbar">
-            <ul>
-              <li><Link to="/index" className="nav-link scrollto">Inicio</Link></li>
-              <li><Link to="/solicitudes" className="nav-link scrollto">Solicitudes</Link></li>
-              <li><Link to="/consultas" className="nav-link scrollto active">Consultas</Link></li>
-              <li><Link to="/simulacionpago"  className="nav-link scrollto">Simulación de pago</Link></li>
-              <li><Link to="/contactenos" className="nav-link scrollto">Contactenos</Link></li>
-              <li><Link to="/dashboard" className="nav-link scrollto">Dashboard</Link></li>
-              <li><Link to="/" className="getstarted scrollto">Salir</Link></li>
-            </ul>
-            <i className="bi bi-list mobile-nav-toggle"></i>
-          </nav>{/*<!-- .navbar -->*/}
-  
-        </div>
-      </header>{/*<!-- End Header -->*/}
-  
+      <BarraSuperior/> 
       <main id="main">
   
        {/* <!-- ======= Breadcrumbs Section ======= -->*/}
@@ -192,6 +170,7 @@ import { AiFillDollarCircle,
           
         </section>{/* <!-- End Contact Section --> */}
       </main>{/* <!-- End #main --> */}
+      <BarraInferior/> 
     </body>)
     }
 }
--- src/pages/Solicitudes.js
+++ src/pages/Solicitudes.js
@@ -7,6 +7,8 @@ import { AiFillDollarCircle,
   AiFillMoneyCollect,
   AiFillBank,
   AiOutlineSearch }  from "react-icons/ai";
+import BarraInferior from "../components/BarraInferior";
+import BarraSuperior from "../components/BarraSuperior";
   function Solicitudes(){
     const [Nombre, setNombre]=useState("");
     const [numbererror,setnumbererror]=useState(0);
@@ -29,33 +31,7 @@ import { AiFillDollarCircle,
     }
   return(
     <body>
-      {/* <!-- ======= Header ======= --> */}
-      <header id="header" className="fixed-top d-flex align-items-center">
-      <div className="container d-flex align-items-center justify-content-between">
-
-        <div className="logo">
-          <Link to="/index"><img src="assets_general/img/citi.png" alt="" className="img-fluid"/></Link>
-          <a style={{fontWeight: "bold", paddingLeft: "30px", paddingTop: "10px"}}>Bienvenido Usuario X</a>
-        </div>
-
-       
-
-        <nav id="navbar" className="navbar">
-          <ul>
-            <li><Link to="/index" className="nav-link scrollto">Inicio</Link></li>
-            <li><Link to="/solicitudes" className="nav-link scrollto active">Solicitudes</Link></li>
-            <li><Link to="/consultas" className="nav-link scrollto">Consultas</Link></li>
-            <li><Link to="/simulacionpago"  className="nav-link scrollto">Simulación de pago</Link></li>
-            <li><Link to="/contactenos" className="nav-link scrollto">Contactenos</Link></li>
-            <li><Link to="/dashboard" className="nav-link scrollto">Dashboard</Link></li>
-            <li><Link to="/" className="getstarted scrollto">Salir</Link></li>
-          </ul>
-          <i className="bi bi-list mobile-nav-toggle"></i>
-        </nav>{/*<!-- .navbar -->*/}
-
-      </div>
-    </header>{/*<!-- End Header -->*/}
-
+    <BarraSuperior/> 
     <main id="main">
 
      {/* <!-- ======= Breadcrumbs Section ======= -->*/}
@@ -128,7 +104,7 @@ import { AiFillDollarCircle,
         </div>
       </section>{/* <!-- End Contact Section --> */}
     </main>{/* <!-- End #main --> */}
-    
+    <BarraInferior/> 
   </body>)
 
   }
